d9014b0f4f45ea3fcf2221dc863334a6e1677342
jaden.liao <jaden.liao@zoom.us>
2019-09-04 16:49:29 +0800

    add get and post

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,36 @@
 const router = require('koa-router')()
+const service = require('../utils/query.js')
 
 router.prefix('/users')
 
-router.get('/', function (ctx, next) {
-  ctx.body = 'this is a users response!'
+router.get('/list', async (ctx, next) => {
+  console.log(ctx)
+  ctx.body = await service.listblog(1);
 })
 
-router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
+router.get('/add', async (ctx, next) => {
+  let arr = [];
+
+  arr.push('koa2');
+  arr.push('xdhc304');
+  arr.push('https://www.jianshu.com/p/b4e36739d85a');
+  arr.push('简书');
+  arr.push(new Date());
+  arr.push(new Date());
+
+  await service.addblog(arr).then((data) => {
+    let r = '';
+    if (data.affectedRows != 0) {
+      r = 'ok';
+    }
+    ctx.body = {
+      data: r
+    }
+  }).catch(() => {
+    ctx.body = {
+      data: 'err'
+    }
+  })
 })
 
 module.exports = router
--- utils/query.js
+++ utils/query.js
@@ -4,26 +4,34 @@ const MYSQL_CONFIG = require('../config/mysql_config') // 数据库配置
 // mysql
 const pool = mysql.createPool(MYSQL_CONFIG)
 
-// query sql语句入口
-const query = (sql, val) => {
-  return new Promise((resolve, reject) => {
-    pool.getConnection(function (err, connection) {
-      if (err) {
-        reject(err)
-      } else {
-        connection.query(sql, val, (err, fields) => {
-          if (err) {
-            reject(err)
-          } else {
-            resolve(fields)
-          }
-          connection.release()
-        })
-      }
+const service = {
+  // query sql语句入口
+  query: (sql, val) => {
+    return new Promise((resolve, reject) => {
+      pool.getConnection(function (err, connection) {
+        if (err) {
+          reject(err)
+        } else {
+          connection.query(sql, val, (err, fields) => {
+            if (err) {
+              reject(err)
+            } else {
+              resolve(fields)
+            }
+            connection.release()
+          })
+        }
+      })
     })
-  })
+  },
+  listblog: (name) => {
+    let _sql = `select * from tb_blog where blog_id="${name}";`
+    return service.query(_sql)
+  },
+  addblog: (obj) => {
+    let _sql = "insert into tb_blog set blog_title=?,blog_author=?,blog_url=?,blog_platform=?,create_time=?,last_edit_time=?;"
+    return service.query(_sql, obj)
+  }
 }
 
-module.exports = {
-  query
-}
+module.exports = service

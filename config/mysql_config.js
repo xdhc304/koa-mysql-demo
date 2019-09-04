f6154c8c4a7051de609f6ace34ea7d89902e3144
jaden.liao <jaden.liao@zoom.us>
2019-09-04 15:43:45 +0800

    create project and add mysql

---
+++ config/mysql_config.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql')
+
+const mysqlConfig= {
+    user: "root",
+    password: "root",
+    database: "xdhc304",
+    host: "localhost",
+    port: 3306
+}
+
+module.exports = mysqlConfig
